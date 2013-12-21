5a2c41f06cd356d27e54c4f79aa3a5cc24cb9622
John Thornborrow <thornborrow.john@gmail.com>
2013-12-21 04:42:16 +0000

    added happy face when no builds returned.

--- public/javascripts/buildRadiator.js
+++ public/javascripts/buildRadiator.js
@@ -2,7 +2,7 @@
 	$(function() {
 		var showAllBuilds = $('#showAllBuilds');
 		var countdown = $('#countdown');
-		var timer = new Timer(5);
+		var timer = new Timer(10);
 		var builds = $('#builds');
 
 		timer.onTick(function(tick) {
@@ -11,21 +11,27 @@
 
 		timer.onCountdownFinished(function() {
 			timer.end();
-			builds.empty().addClass('loading');
+			builds.empty().removeClass('all-clear').addClass('loading');
 
 
-			var resume = function() {
+			var resume = function() {	
 				builds.removeClass('loading');
 				timer.begin();
 			};
 
 			var renderBuildsList = function(data) {
 				var buildsList = $('<ul></ul>');
-				$(data).each(function(index, build) {
-					buildsList.append(buildToListItem(build));
-				});
-				buildsList.appendTo(builds);
-				resume();
+
+				if (data.length) {
+					$(data).each(function(index, build) {
+						buildsList.append(buildToListItem(build));
+					});
+					buildsList.appendTo(builds);
+					resume();
+				} else {
+					resume();
+					builds.addClass('all-clear');
+				}
 			};
 
 			var displayErrorMessage = function(jqXhr, textStatus, errorThrown) {
@@ -42,9 +48,7 @@
 				url: url,
 				success: renderBuildsList,
 				error: displayErrorMessage,
-				headers: {
-					'Cache-Control': 'no-store, no-cache, must-revalidate' 
-				}
+				headers: { 'Cache-Control': 'no-store, no-cache, must-revalidate' }
 			});
 		});
 
